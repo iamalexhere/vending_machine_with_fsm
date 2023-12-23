b8efe37b84c0aba83b3cfe1a23d7dc8918e710a9
alexhere <>
2023-12-23 10:52:49 +0700

    First Commit!

---
+++ app/src/main/java/sazlm/vending_model/Machine.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.Machine to edit this template
+ */
+package sazlm.vending_model;
+
+/**
+ *
+ * @author ialex
+ */
+public class Machine {
+    
+}
